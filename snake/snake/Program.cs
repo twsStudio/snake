c821213933e2a3d857bb29100128fe445ad7452c
Andey <tws.zp.ua@gmail.com>
2015-12-27 23:00:46 +0200

    7 урок, навписана вертикальная линия

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,11 +10,21 @@ namespace snake
     {
         static void Main(string[] args)
         {
+            
             Point p1 = new Point(1, 3, '#');
             p1.Draw();
 
             Point p2 = new Point(2, 4, '*');
             p2.Draw();
+           
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '#');
+            line.Drow(); 
+
+
+
+            VerticalLine lineV = new VerticalLine(5, 10, 8, '*');
+            lineV.Drow();
+
 
             Console.ReadLine();
         }
